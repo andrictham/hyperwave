4af2d3dc4f99a69b635cd1bfd8fee081110141db
Andric Tham <andrictham@gmail.com>
2025-06-12 19:17:44 +0800

    chore(prettier): Setup Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,29 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: "always",
+  printWidth: 100,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^~/utils/(.*)$",
+    "^~/components/(.*)$",
+    "^~/styles/(.*)$",
+    "^~/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+};
+
+module.exports = config;
