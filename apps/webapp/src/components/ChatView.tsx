1ba15175699b236fc83e6d17b99943b5284acf36
Andric Tham <andrictham@gmail.com>
2025-06-14 14:37:23 +0800

    fix: Do not hide SidebarTrigger on md

--- apps/webapp/src/components/ChatView.tsx
+++ apps/webapp/src/components/ChatView.tsx
@@ -11,7 +11,7 @@ export function ChatView() {
         {/* Top Bar */}
         <header className="h-[60px] flex items-center px-4 md:px-8 backdrop-blur-md sticky top-0 z-10">
           <div className="flex items-center gap-4">
-            <SidebarTrigger className="md:hidden" />
+            <SidebarTrigger className="-ml-1" />
             <div className="bg-[#c5420a] text-[#fffdfc] flex items-center justify-center rounded-md w-8 h-8 font-bold text-lg shadow-lg">
               H
             </div>
