2f55b97851080037397862654ac817620b936463
Andric Tham <andrictham@gmail.com>
2025-06-15 20:02:02 +0800

    style: Hide extra user settings that are unused in the demo version

--- apps/webapp/src/components/nav-user.tsx
+++ apps/webapp/src/components/nav-user.tsx
@@ -53,11 +53,12 @@ export function NavUser({
           </DropdownMenuTrigger>
           <DropdownMenuContent
             className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
+            side={isMobile ? "top" : "top"}
             align="end"
             sideOffset={4}
           >
-            <DropdownMenuLabel className="p-0 font-normal">
+            {/* TODO: Add more user settings */}
+            {/* <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.name} />
@@ -91,8 +92,8 @@ export function NavUser({
                 Notifications
               </DropdownMenuItem>
             </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={() => void signOut()}>
+            <DropdownMenuSeparator /> */}
+            <DropdownMenuItem onClick={() => void signOut()} variant="destructive">
               <LogOut />
               Log out
             </DropdownMenuItem>
